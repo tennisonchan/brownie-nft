fc1f6544ef13057ffabf8ddf152e8c7056eeca0f
Tennison Chan <tennison.chan+gh@gmail.com>
2021-12-29 00:47:27 -0800

    Add create_collectible function

--- scripts/advanced_collectible/deploy.py
+++ scripts/advanced_collectible/deploy.py
@@ -1,40 +1,29 @@
 from brownie import AdvancedCollectible, network, config
 from scripts.helpers import (
     get_account,
-    OPEN_SEA_TEST_NET_URL,
     get_contract,
-    fund_contract,
 )
-
-agumon_token_uri = "https://ipfs.io/ipfs/bafkreifgmuhcggw7c5rtn2q4ro5jyxo67rw5oktabku26j6hkslcvr76uy?filename=augmon.json/"
 
 
 def deploy_collectible():
-    if len(AdvancedCollectible) == 0:
-        # vrfCoordinator: 0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B
-        # linkToken: 0x01BE23585060835E02B77ef475b0Cc51aA1e0709
-        # keyHash: 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311
-        # fee 0.1 LINK
-        # https://docs.chain.link/docs/vrf-contracts/#rinkeby
-        return AdvancedCollectible.deploy(
-            get_contract("vrf_coordinator"),
-            get_contract("link_token"),
-            config["networks"][network.show_active()]["key_hash"],
-            config["networks"][network.show_active()]["fee"],
-            {"from": get_account()},
-        )
-    return AdvancedCollectible[-1]
-
-
-def create_collectible():
-    account = get_account()
-    contract = deploy_collectible()
-    fund_contract(contract.address)
-    tx = contract.createCollectible({"from": account})
-    tx.wait(1)
-    return tx
+    # vrfCoordinator: 0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B
+    # linkToken: 0x01BE23585060835E02B77ef475b0Cc51aA1e0709
+    # keyHash: 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311
+    # fee 0.1 LINK
+    # https://docs.chain.link/docs/vrf-contracts/#rinkeby
+    print("Deploying...")
+    contract = AdvancedCollectible.deploy(
+        get_contract("vrf_coordinator"),
+        get_contract("link_token"),
+        config["networks"][network.show_active()]["key_hash"],
+        config["networks"][network.show_active()]["fee"],
+        {"from": get_account()},
+        publish_source=config["networks"][network.show_active()].get("verify", False),
+    )
+    print("Deployed!")
+    return contract
 
 
 def main():
     print(f"Run on network: {network.show_active()}")
-    create_collectible()
+    deploy_collectible()
